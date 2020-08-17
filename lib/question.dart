1c025c97617b16898bb3514bad882596c28ab602
Asif <alli.asif@gmail.com>
2020-08-16 23:58:14 -0400

    container props

--- lib/question.dart
+++ lib/question.dart
@@ -7,6 +7,12 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(_questionText);
+    return Container(
+        width: double.infinity,
+        child: Text(
+          _questionText,
+          style: TextStyle(fontSize: 28),
+          textAlign: TextAlign.center,
+        ));
   }
 }
